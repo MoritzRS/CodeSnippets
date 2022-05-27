4c3d6b02ada22ea47bbb2fb91896faf832e08377
MoritzRS <moritz.r.schulz@gmail.com>
2022-05-27 17:35:23 +0200

    [layout] added persistent theme changes

--- src/lib/components/layout/sidebar/ThemeSwitcherButton.svelte
+++ src/lib/components/layout/sidebar/ThemeSwitcherButton.svelte
@@ -2,16 +2,16 @@
 	import MoonIcon from "$lib/icons/MoonIcon.svelte";
 	import SunIcon from "$lib/icons/SunIcon.svelte";
 	import { SETTINGS, SettingManager } from "$lib/modules/settings/settings";
+
+	function toggleMode() {
+		SettingManager.toggleDarkMode();
+		SettingManager.saveSettings();
+	}
 </script>
 
 <template>
 	<div class="w-16 h-16">
-		<button
-			type="button"
-			class="btn btn-ghost w-full h-full"
-			title="Toggle Color Theme"
-			on:click={SettingManager.toggleDarkMode}
-		>
+		<button type="button" class="btn btn-ghost w-full h-full" title="Toggle Color Theme" on:click={toggleMode}>
 			{#if $SETTINGS.darkMode}
 				<MoonIcon size={48} />
 			{:else}
