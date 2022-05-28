852abd7c512da27d654376f130cb717a4ed03535
MoritzRS <moritz.r.schulz@gmail.com>
2022-05-28 11:32:07 +0200

    [editor] added extended language support

---
+++ src/lib/components/editor/snippet/LanguageSelector.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { supportedLanguages } from "$lib/modules/languages";
+
+	export let language: string;
+</script>
+
+<template>
+	<select class="select select-bordered" bind:value={language}>
+		{#each supportedLanguages as language}
+			<option value={language.id}>{language.name}</option>
+		{/each}
+	</select>
+</template>
+
+<style lang="scss">
+</style>
--- src/lib/components/editor/snippet/Snippet.svelte
+++ src/lib/components/editor/snippet/Snippet.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { Snippet } from "$lib/modules/types";
+	import LanguageSelector from "./LanguageSelector.svelte";
 	import Monaco from "./Monaco.svelte";
 
 	export let snippet: Snippet;
@@ -9,10 +10,7 @@
 	<div class="w-full max-w-5xl border">
 		<div class="w-full p-2 flex flex-row justify-between">
 			<span>{snippet.title}</span>
-			<select class="select select-bordered" bind:value={snippet.language}>
-				<option value="javascript">javascript</option>
-				<option value="typescript">typescript</option>
-			</select>
+			<LanguageSelector bind:language={snippet.language} />
 		</div>
 		<div class="w-full p-4">
 			<Monaco {snippet} />
