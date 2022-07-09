a994bb4bbac6dc8248184ee89eb1f104c19b0372
MoritzRS <moritz.r.schulz@gmail.com>
2022-07-09 15:11:48 +0200

    changed title and icon

--- src/lib/components/helloworld/HelloWorld.svelte
+++ src/lib/components/helloworld/HelloWorld.svelte
@@ -6,7 +6,7 @@
 	<div class="w-full h-full flex flex-col items-center justify-center">
 		<div class="card card-bordered bg-base-200">
 			<div class="card-body">
-				<h1 class="text-6xl font-bold text-center">codeSnippets</h1>
+				<h1 class="text-6xl font-bold text-center">CodeSnippets</h1>
 
 				<div class="divider">By Moritz R. S. and Lucas H.</div>
 
